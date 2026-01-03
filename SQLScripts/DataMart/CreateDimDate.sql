392f5eec6af2f0b61b369dbb4212a073498ad612
Jorge Cruz <jorge_cruz_6@hotmail.com>
2026-01-03 14:28:29 +0000

    DimDate loaded. Logical validations added SQLScript for DimDate correction. DimDate base csv added.

--- SQLScripts/DataMart/CreateDimDate.sql
+++ SQLScripts/DataMart/CreateDimDate.sql
@@ -1,22 +1,21 @@
 IF NOT EXISTS (SELECT name FROM sys.tables WHERE name = 'DimDate')
 BEGIN
     CREATE TABLE [dbo].[DimDate](
-        [DateKey]          [int]         NOT NULL,
+        [DateKey] [int] IDENTITY(1,1)    NOT NULL,
         [FullDate]         [date]        NULL,
         [Year]             [int]         NULL,
         [Month]            [tinyint]     NULL,
-        [Day]              [tinyint]     NULL,
         [DayNumberOfYear]  [int]         NULL,
         [DayNumberOfMonth] [tinyint]     NULL,
         [DayNumberOfWeek]  [tinyint]     NULL,
         [Week]             [tinyint]     NULL,
-        [DayOfWeek]        [int]         NULL,
-        [Weekend]          [varchar](3)  NULL,
-        [MonthName]        [varchar](10) NULL,
+        [DayOfWeek]        [nvarchar](10)        NULL,
+        [Weekend]          [nvarchar](10)  NULL,
+        [MonthName]        [nvarchar](10) NULL,
         [Quarter]          [tinyint]     NULL,
         [Semester]         [tinyint]     NULL,
         [Trimester]        [tinyint]     NULL,
-        [Season]           [tinyint]     NULL,
+        [Season]           [nvarchar](10)     NULL,
         CONSTRAINT [PK_DimDate] PRIMARY KEY CLUSTERED ([DateKey] ASC)
     );
 END
