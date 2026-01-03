6c2f1834fabe8aa9a321d10f30dd30a2083052b9
Jorge Cruz <jorge_cruz_6@hotmail.com>
2026-01-03 18:22:27 +0000

    DimSite loaded

--- SQLScripts/DataMart/CreateDimSite.sql
+++ SQLScripts/DataMart/CreateDimSite.sql
@@ -3,13 +3,13 @@ BEGIN
     CREATE TABLE [dbo].[DimSite](
         [SiteKey]       [int] IDENTITY(1,1)    NOT NULL,
         [SiteID]        [int]          NOT NULL,
-        [SiteName]      [nvarchar](50)  NULL,
-        [SiteURL]       [nvarchar](50)  NULL,
+        [SiteName]      [nvarchar](255)  NULL,
+        [SiteURL]       [nvarchar](255)  NULL,
         [SiteInitials]  [nvarchar](10)     NULL,
         [IsSite]        [nvarchar](10)     NULL,
         [EffectiveDate] [date]         NULL,
         [ExpiredDate]   [date]         NULL,
-        [IsCurrent]     [tinyint]      NULL,
+        [IsCurrent]     [nvarchar](3)      NULL,
         CONSTRAINT [PK_DimSite] PRIMARY KEY CLUSTERED ([SiteKey] ASC)
     );
 END
